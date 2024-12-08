3dc98e8994bcae94dd200ca776dad637c0eb6838
Ramys <limajosimar@yahoo.com.br>
2024-12-08 12:16:38 -0300

    Create MP_loader.cpp

---
+++ src/MP_loader.cpp
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU GPLv2+ license
+ * Written by Nefertumm : https://github.com/Nefertumm
+ */
+
+// From SC
+void AddCustomDeserterScripts();
+
+// Add all
+// cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
+// additionally replace all '-' in the module folder name with '_' here
+void Addmod_custom_deserterScripts()
+{
+    AddCustomDeserterScripts();
+}
